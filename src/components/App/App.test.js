8c18575db5c2167a8d0d7de93efcc5bf293f11ec
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-05 22:56:37 -0600

    Finish test coverage for App and NewsArticle Components

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -16,10 +16,54 @@ describe("App", () => {
   });
 
   it("should update state upon change data being invoked", () => {
-    const mockEvent = { target: { id: 'science' } };
+    const mockEvent = { target: { id: "science" } };
     const expectedCategory = science;
     wrapper.instance().changeData(mockEvent);
 
     expect(wrapper.state("current")).toEqual(expectedCategory);
+  });
+
+  it("should search articles in current category and display search results", () => {
+    const mockSearchedTerm = "nasa";
+    wrapper.setState({
+      current: science,
+      displayed: science
+    });
+    wrapper.instance().searchArticles(mockSearchedTerm);
+
+    expect(wrapper.state("displayed")).toEqual([
+      {
+        description:
+          "The image was captured by NASA's Lunar Reconnaissance Orbiter (LRO) on Sept. 17 as it flew over the targeted landing site of India's Chandrayaan-2 mission.",
+        headline: "India's Lost Moon Lander Is Somewhere in This Photo",
+        id: 1,
+        img:
+          "https://cdn.mos.cms.futurecdn.net/FsUeVGmY89BWeb36U8cUj5-650-80.jpg",
+        url:
+          "https://www.livescience.com/india-moon-landing-site-nasa-photo.html"
+      },
+      {
+        description:
+          "NASA's black hole experts popped up on Reddit on Friday to field questions about these enigmatic objects. ",
+        headline:
+          "NASA, in Reddit AMA, says Earth probably won't be eaten by a black hole",
+        id: 2,
+        img:
+          "https://cnet2.cbsistatic.com/img/tM8V3SxstaN-YQiY1QPVMhYBF1k=/2019/09/26/f41e8d77-b008-4887-ae0b-a6dba3e4db60/bh-visualization1.jpg",
+        url:
+          "https://www.cnet.com/news/nasa-in-reddit-ama-says-earth-probably-wont-be-eaten-by-a-black-hole/"
+      },
+      {
+        description:
+          "Lockheed Martin has won a contract from NASA to produce and operate six Orion spacecraft missions, a program the space agency calls the “backbone” of its plans for deep-space exploration and one that will send the first woman to the moon.",
+        headline:
+          "Lockheed Martin wins NASA contract for Orion program, which will send first woman to the moon",
+        id: 5,
+        img:
+          "https://www.denverpost.com/wp-content/uploads/2018/04/lockheed-04-16-2018-ja-_ja28138a.jpg?w=863",
+        url:
+          "https://www.denverpost.com/2019/09/27/jefferson-county-lockheed-martin-orion-nasa/"
+      }
+    ]);
   });
 });
--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
-import NewsArticle from './NewsArticle'
+import NewsArticle from "./NewsArticle";
 
 describe("NewsArticle", () => {
   it("should match the snapshot", () => {
     let article = {
-      description: '',
-      headline: '',
+      description: "",
+      headline: "",
       id: 1,
-      img: '',
-      url: ''
-    }
+      img: "",
+      url: ""
+    };
     const wrapper = shallow(
       <NewsArticle
         key={article.id}
@@ -18,7 +18,8 @@ describe("NewsArticle", () => {
         description={article.description}
         url={article.url}
         img={article.img}
-      />)
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
