b2ac87857dde9f072b8ec9b7f805a8330461e50c
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-06 21:09:31 -0600

    Updates

---
+++ precache-manifest.e52e95d8b7b4a0c3c8e7e197a83466b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceeb13069cf62a80c545edab1721b2dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0944a20bf86525bb0b95",
+    "url": "/static/css/main.0a875a2e.chunk.css"
+  },
+  {
+    "revision": "e1e35dfba66e49e176a5",
+    "url": "/static/js/2.e5cdc2de.chunk.js"
+  },
+  {
+    "revision": "0944a20bf86525bb0b95",
+    "url": "/static/js/main.4e58a3b7.chunk.js"
+  },
+  {
+    "revision": "25612ba57795dcc22c86",
+    "url": "/static/js/runtime-main.ae1c00e5.js"
+  }
+]);
